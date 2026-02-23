2a4294f332801df03724d1afd6c17908901d0842
SanjayCANDA <candasanjay1@gmail.com>
2026-02-23 12:03:14 +0100

    black file

--- tests/test_simple_calculator.py
+++ tests/test_simple_calculator.py
@@ -5,9 +5,8 @@ Date : 12/02/2026
 Objectif : Fichier test pour calculator, permet de vérifier le bon fonctionnement de calculator
 """
 
-
-
 from calculator.simple_calculator import Calculator
+
 calc = Calculator()
 
 
@@ -17,7 +16,7 @@ Som = 0
 
 # Test résultat correct
 try:
-    if calc.fsum(5,5) == 10:
+    if calc.fsum(5, 5) == 10:
         Som += 1
     else:
         print("Problème de calcul")
@@ -26,7 +25,7 @@ except Exception as e:
 
 # Test entrée string
 try:
-    calc.fsum(5,"a")
+    calc.fsum(5, "a")
 except TypeError:
     Som += 1
 else:
@@ -34,7 +33,7 @@ else:
 
 # Test entrée float
 try:
-    calc.fsum(5,1.1)
+    calc.fsum(5, 1.1)
 except TypeError:
     Som += 1
 else:
@@ -46,7 +45,7 @@ nb_test_sous = 3
 Sous = 0
 
 try:
-    if calc.substract(5,5) == 0:
+    if calc.substract(5, 5) == 0:
         Sous += 1
     else:
         print("Problème de calcul")
@@ -54,12 +53,12 @@ except Exception as e:
     print("Erreur inattendue :", e)
 
 try:
-    calc.substract(5,"a")
+    calc.substract(5, "a")
 except TypeError:
     Sous += 1
 
 try:
-    calc.substract(5,1.1)
+    calc.substract(5, 1.1)
 except TypeError:
     Sous += 1
 
@@ -69,7 +68,7 @@ nb_test_mult = 3
 Mult = 0
 
 try:
-    if calc.multiply(5,5) == 25:
+    if calc.multiply(5, 5) == 25:
         Mult += 1
     else:
         print("Problème de calcul")
@@ -77,12 +76,12 @@ except Exception as e:
     print("Erreur inattendue :", e)
 
 try:
-    calc.multiply(5,"a")
+    calc.multiply(5, "a")
 except TypeError:
     Mult += 1
 
 try:
-    calc.multiply(5,1.1)
+    calc.multiply(5, 1.1)
 except TypeError:
     Mult += 1
 
@@ -93,7 +92,7 @@ Div = 0
 
 # Résultat correct
 try:
-    if calc.divide(5,5) == 1:
+    if calc.divide(5, 5) == 1:
         Div += 1
     else:
         print("Problème de calcul")
@@ -102,26 +101,26 @@ except Exception as e:
 
 # Entrée string
 try:
-    calc.divide(5,"a")
+    calc.divide(5, "a")
 except TypeError:
     Div += 1
 
 # Entrée float
 try:
-    calc.divide(5,1.1)
+    calc.divide(5, 1.1)
 except TypeError:
     Div += 1
 
 # Division 0/b
 try:
-    if calc.divide(0,5) == 0:
+    if calc.divide(0, 5) == 0:
         Div += 1
 except Exception as e:
     print("Erreur inattendue :", e)
 
 # Division par zéro
 try:
-    calc.divide(5,0)
+    calc.divide(5, 0)
 except ZeroDivisionError:
     Div += 1
 else:
@@ -130,11 +129,11 @@ else:
 
 # Calcul des pourcentages
 print("\n========= RESULTATS TESTS ========")
-print("Test Somme : ", (Som/nb_test_som)*100, "%")
-print("Test Soustraction : ", (Sous/nb_test_sous)*100, "%")
-print("Test Multiplication : ", (Mult/nb_test_mult)*100, "%")
-print("Test Division : ", (Div/nb_test_div)*100, "%")
+print("Test Somme : ", (Som / nb_test_som) * 100, "%")
+print("Test Soustraction : ", (Sous / nb_test_sous) * 100, "%")
+print("Test Multiplication : ", (Mult / nb_test_mult) * 100, "%")
+print("Test Division : ", (Div / nb_test_div) * 100, "%")
 
 total_success = Som + Sous + Mult + Div
 total_tests = nb_test_som + nb_test_sous + nb_test_mult + nb_test_div
-print("\nRésultat global : ", (total_success/total_tests)*100, "%")
+print("\nRésultat global : ", (total_success / total_tests) * 100, "%")
